10a0ad441fc7d948b5183afe5a093b353fedac0b
Arjay Fuentes <arjay@gmail.com>
2025-01-07 22:46:08 +0100

    initial commit

---
+++ src/main/java/com/arjayprojects/lakbay/LakbayApplication.java
@@ -0,0 +1,13 @@
+package com.arjayprojects.lakbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjayprojects/lakbay/LakbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjayprojects.lakbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
